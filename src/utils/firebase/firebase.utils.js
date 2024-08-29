9e2a83508e7900182f0872ec556685d1dd3d21d9
AfsanehSharifian <135505635+AfsanehSharifian@users.noreply.github.com>
2024-08-29 11:43:59 +0330

    putting my own firebase config

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyASr5pKicY8p6KrowI0ePG78dCv6vlhrXs",
+  authDomain: "crwn-db-dfe90.firebaseapp.com",
+  projectId: "crwn-db-dfe90",
+  storageBucket: "crwn-db-dfe90.appspot.com",
+  messagingSenderId: "92304823346",
+  appId: "1:92304823346:web:e3bdf19faae12cf407582b",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
